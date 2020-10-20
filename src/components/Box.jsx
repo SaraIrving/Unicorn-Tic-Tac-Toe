ded1ff27f7934505ad91f0fa6a40162576826e5f
Sara <sarakirving@gmail.com>
2020-10-20 14:30:24 -0700

    create single Box component that will be rendered in the GameBoard component, should have a function that handles which image to display when clicked depending on which players turn it is, which will be stord in state, currently function does not do this, instead it is playing iwth which playerIcon to display but it will be refactored

---
+++ src/components/Box.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Corgi from './Corgi'
+import Unicorn from './Unicorn'
+
+export default function Box (props) {
+
+  function handleBoxClick () {
+    if (props.image === "U") {
+      return <Unicorn/>
+    } else if (props.image === "N") {
+      return null;
+    }
+  }
+
+  return (
+  <button >{handleBoxClick()}</button>
+  )
+
+
+
+
+}
