9d0049dca552c2d1db57ced60b4a2c2e11b0b40e
Sara <sarakirving@gmail.com>
2020-10-20 15:12:58 -0700

    experiment with mapping through an array that will dictate what is rendered in the Box components that make up the GameBoard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Box from './Box'
+
+export default function GameBoard (props) {
+
+  let boardArray = [["U", "U", "N"], ["N", "N", "N"], ["N", "N", "N"]]
+  let noNestArray = ["U", "N", "N", "N", "N", "N", "N", "N", "N"];
+
+  return (
+    <div>
+      {noNestArray.map(box => {
+        if (box === "U") {
+          return (<Box image="U"></Box>)
+        } else if (box === "N") {
+          return (<Box image="N"></Box>)
+        }
+        return (<p>A ROW!</p>)
+       
+    })}
+    </div>
+  )
+
+
+}
