aebd74405a7820b9ec79c8394170f5cee6dd1911
Sara <sarakirving@gmail.com>
2020-10-20 16:32:20 -0700

    refactor so Box component displays whichever image is related to the corresponding index in the gameBoard array in gameState

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import Corgi from './Corgi'
-import Unicorn from './Unicorn'
+import React from 'react';
+import Corgi from './Corgi';
+import Unicorn from './Unicorn';
+import useGameState from '../hooks/useGameState';
 
 export default function Box (props) {
 
+  const {gameState, setGameState} = useGameState();
+
   function handleBoxClick () {
-    if (props.image === "U") {
+    // if (props.image === "U") {
+    //   return <Unicorn/>
+    // } else if (props.image === "N") {
+    //   return null;
+    // }
+
+    //setGameState(prev => {return {...prev, unicornTurn: false}})
+
+    if (gameState.unicornTurn) {
       return <Unicorn/>
-    } else if (props.image === "N") {
-      return null;
     }
+
+    
+
+
   }
 
   return (
-  <button className="boxButton">{handleBoxClick()}</button>
+    <div>
+      {props.image === "U" && <button className="boxButton"><Unicorn/></button>}
+      {props.image === "N" && <button className="boxButton"></button>}
+
+    </div>
+
+    // <button className="boxButton"></button>
+  
   )
 
 
