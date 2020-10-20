7408a1ea7480c0981711fcdb5c80cba645617174
Sara <sarakirving@gmail.com>
2020-10-20 16:56:44 -0700

    refactor so it builds the board based off the values in the gameBoard array in gameState

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Box from './Box'
+import useGameState from '../hooks/useGameState';
 
 export default function GameBoard (props) {
 
   let boardArray = [["U", "U", "N"], ["N", "N", "N"], ["N", "N", "N"]]
   let noNestArray = ["U", "N", "N", "N", "N", "N", "N", "N", "N"];
 
+  const {gameState, setGameState} = useGameState();
+
   return (
     <div className="gameBoardWrapper">
-      {noNestArray.map(box => {
+      {gameState.gameBoard.map(box => {
         if (box === "U") {
           return (<Box image="U"></Box>)
         } else if (box === "N") {
