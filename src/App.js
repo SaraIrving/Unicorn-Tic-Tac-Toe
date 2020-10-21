95d617fe209a22151393fbff4bad2c1deb8ccb25
Sara <sarakirving@gmail.com>
2020-10-21 11:14:52 -0700

    add button to reset the board, may remove this after scoring is implemented

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function App() {
 
   const {gameState, setGameState} = useGameState();
 
+  const gameBoardReset = [{index: 0, display: "N"}, 
+  {index: 1, display: "N"}, 
+  {index: 2, display: "N"}, 
+  {index: 3, display: "N"}, 
+  {index: 4, display: "N"}, 
+  {index: 5, display: "N"}, 
+  {index: 6, display: "N"}, 
+  {index: 7, display: "N"}, 
+  {index: 8, display: "N"}];
+
 
 
   return (
@@ -30,6 +40,7 @@ function App() {
         </div>
         <p>The Score is: Unicorn: <span>{gameState.score.unicorn}</span> Corgi: <span>{gameState.score.corgi}</span></p>
         <button onClick={event => setGameState(prev => {return {...prev, score: {unicorn: 0, corgi:0}}})}>RESET THE SCORE!</button>
+        <button onClick={event => setGameState(prev => {return {...prev, gameBoard: gameBoardReset}})}>Play Again!</button>
         <Corgi/>
         <Unicorn/>
         <GameBoard gameState={gameState} setGameState={setGameState}/>
