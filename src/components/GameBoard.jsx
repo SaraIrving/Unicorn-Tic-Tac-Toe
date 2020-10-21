76b91e5eb2522facb6591f672ce8f8a654cb031c
Sara <sarakirving@gmail.com>
2020-10-21 09:55:31 -0700

    refactor to use new formatting of gameBoard in state, assign each Box component a unique key which is the index value from state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -9,13 +9,20 @@ export default function GameBoard (props) {
 
   //const {gameState, setGameState} = useGameState();
 
+  //need to work out how to give each Box an individual key that we can use to relate it with the index of the letter that it represents in the gameBoard array in state, maybe make game board array an array of objects with key value pairs of a numerical key and a alphabetical value??
+
+
+
   return (
     <div className="gameBoardWrapper">
-      {props.gameState.gameBoard.map(box => {
-        if (box === "U") {
-          return (<Box image="U"></Box>)
-        } else if (box === "N") {
-          return (<Box image="N" gameState={props.gameState} setGameState={props.setGameState}></Box>)
+      {props.gameState.gameBoard.map(({index, display}) => {
+        
+        if (display === "U") {
+          return (<Box image={display} key={index} gameState={props.gameState} setGameState={props.setGameState}></Box>)
+        } else if (display === "N") {
+          return (<Box image={display} key={index} gameState={props.gameState} setGameState={props.setGameState}></Box>)
+        } else if (display === "C") {
+          return (<Box image={display} key={index} gameState={props.gameState} setGameState={props.setGameState}></Box>)
         }
         return (<p>A ROW!</p>)
        
