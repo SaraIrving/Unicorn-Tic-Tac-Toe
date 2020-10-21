01c5869bb1ee9a69fea2b75e0af9b0a93eeba973
Sara <sarakirving@gmail.com>
2020-10-21 11:07:52 -0700

    create updateGameBoard function which changes the display value in the corresponding index of the gameBoard array in state to reflect which player just clicked on the Box. This is also used to determine which image to display on the Box after it is clicked

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -51,30 +51,33 @@ export default function Box (props) {
   }
   const turn = determineTurn();
   
-  function determineImage () {
-    // if (props.image === "N") {
-    //   return "NULL";
-    // }
-    console.log('props = ', props)
-    console.log('props.gameState.gameBoard = ', props.gameState.gameBoard)
-    console.log('props.id = ', props.id)
-    console.log(props.gameState.gameBoard[props.id])
-
+  function determineBoxImage () {
     if(props.gameState.gameBoard[props.id].display === "U") {
       return <Unicorn/>;
     } else if (props.gameState.gameBoard[props.id].display === "C") {
       return <Corgi/>;
     } 
   }
+  const boxImage = determineBoxImage();
 
-  const boxImage = determineImage();
-  console.log('boximage = ', boxImage);
+  function updateGameBoard() {
+    //get the current gameBoar
+    let updatedGameBoard = props.gameState.gameBoard.slice();
+    let currentPlayer = props.gameState.unicornTurn ? "U" : "C";
+    console.log('currnetPlayer = ', currentPlayer)
+    updatedGameBoard[props.id] = {...updatedGameBoard[props.id], display: currentPlayer}
+    return updatedGameBoard;
+  }
+
+  // let newArray = updateGameBoard();
+  // console.log("newArray = ", newArray);
+  
 
   return (
     <div>
       {props.image === "U" && <button className="boxButton" onClick={event =>handleBoxClick()}><Unicorn/></button>}
       {props.image === "C" && <button className="boxButton" onClick={event =>handleBoxClick()}><Corgi/></button>}
-  {props.image === "N" && <button className="boxButton" onClick={event => props.setGameState(prev => {return {...prev, unicornTurn: turn}})}>{boxImage}</button>}
+  {props.image === "N" && <button className="boxButton" onClick={event => props.setGameState(prev => {return {...prev, unicornTurn: turn, gameBoard: updateGameBoard()}})}>{boxImage}</button>}
 
     </div>
 
