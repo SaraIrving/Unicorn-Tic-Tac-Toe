5494f12612be5a0ca63c6a73ae760b72d822a484
Sara <sarakirving@gmail.com>
2020-10-21 09:50:03 -0700

    change the inital structure of the gameBoard array in state so it is an array of objects that contain and index and a display value

--- src/hooks/useGameState.js
+++ src/hooks/useGameState.js
@@ -4,7 +4,15 @@ import { useState, useEffect } from "react";
 export default function useGameState () {
 
   const [gameState, setGameState] = useState({unicornTurn: true, 
-                                              gameBoard: ["N", "N", "N", "N", "N", "N", "N", "N", "N"], 
+                                              gameBoard: [{index: 0, display: "N"}, 
+                                                          {index: 1, display: "N"}, 
+                                                          {index: 2, display: "N"}, 
+                                                          {index: 3, display: "N"}, 
+                                                          {index: 4, display: "N"}, 
+                                                          {index: 5, display: "N"}, 
+                                                          {index: 6, display: "N"}, 
+                                                          {index: 7, display: "N"}, 
+                                                          {index: 8, display: "N"}], 
                                               score: {unicorn: 1, corgi:1}});
 
 
