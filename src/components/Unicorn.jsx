707b571878a8f109c5f137c418b659342669d2b8
Sara <sarakirving@gmail.com>
2020-10-28 16:38:31 -0700

    experiment iwth image source to fix problem with images not loading now that app is hosted on gh pages

--- src/components/Unicorn.jsx
+++ src/components/Unicorn.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Unicorn () {
 
   return (
-    <img className="playerIcon" src="../images/unicorn.png" alt="Unicorn" />
+    // <img className="playerIcon" src="../images/unicorn.png" alt="Unicorn" />
+    <img className="playerIcon" src="https://sarairving.github.io/public/images/unicorn.png" alt="Unicorn" />
   )
 
 }
