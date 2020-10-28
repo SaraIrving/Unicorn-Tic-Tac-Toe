fdedaad594119e6f0432ae730893064541be91fe
Sara <sarakirving@gmail.com>
2020-10-28 16:26:50 -0700

    Updates

---
+++ precache-manifest.a7a14a1b7e754dc67274467de7dc3f67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6241470403113a4f05e775915566bca3",
+    "url": "/Unicorn-Tick-Tack-Toe/index.html"
+  },
+  {
+    "revision": "288f2e60b1e1b3e315ea",
+    "url": "/Unicorn-Tick-Tack-Toe/static/css/main.0458d6d4.chunk.css"
+  },
+  {
+    "revision": "ffaedaac34ac4e30ddcf",
+    "url": "/Unicorn-Tick-Tack-Toe/static/js/2.587c8b49.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Unicorn-Tick-Tack-Toe/static/js/2.587c8b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288f2e60b1e1b3e315ea",
+    "url": "/Unicorn-Tick-Tack-Toe/static/js/main.d6c336ab.chunk.js"
+  },
+  {
+    "revision": "a058c38fb2381a1d9222",
+    "url": "/Unicorn-Tick-Tack-Toe/static/js/runtime-main.ace5258f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Unicorn-Tick-Tack-Toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
