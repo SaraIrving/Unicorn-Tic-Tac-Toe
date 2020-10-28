6f1aa166e638324161c69330d603b3bda3f4f33e
Sara <sarakirving@gmail.com>
2020-10-28 13:42:06 -0700

    removed word tracker from the PlayerTracker text

--- src/components/PlayerTracker.jsx
+++ src/components/PlayerTracker.jsx
@@ -10,8 +10,8 @@ export default function PlayerTracker (props) {
 
   return (
     <div className="playerWrapper">
-    {(props.gameState.unicornTurn === true && props.gameState.winnerAnnouncement === null) && <div><p>IT'S THE UNICORNS TURN tracker!</p><Unicorn/></div>}
-    {(props.gameState.unicornTurn === false && props.gameState.winnerAnnouncement === null) && <div><p>IT'S THE CORGIS TURN tracker !</p><Corgi/></div>}
+    {(props.gameState.unicornTurn === true && props.gameState.winnerAnnouncement === null) && <div><p>IT'S THE UNICORNS TURN!</p><Unicorn/></div>}
+    {(props.gameState.unicornTurn === false && props.gameState.winnerAnnouncement === null) && <div><p>IT'S THE CORGIS TURN!</p><Corgi/></div>}
   </div>
   )
 
