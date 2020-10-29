a93dd853ac17db0a8f0da8b68c02cb05fe0f72ad
Sara <sarakirving@gmail.com>
2020-10-28 17:03:00 -0700

    change how image is referenced so it shows up when hosted on gh pages

--- src/components/Unicorn.jsx
+++ src/components/Unicorn.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Unicorn () {
 
   return (
-    // <img className="playerIcon" src="../images/unicorn.png" alt="Unicorn" />
-    <img className="playerIcon" src="/Unicorn-Tick-Tack-Toe/images/unicorn.png" alt="Unicorn" />
+    <img className="playerIcon" src="./images/unicorn.png" alt="Unicorn" />
+    // <img className="playerIcon" src="/Unicorn-Tick-Tack-Toe/images/unicorn.png" alt="Unicorn" />
   )
 
 }
