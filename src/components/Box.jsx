548a2b97e565d3e769cce064ae8de98edfde4ed8
Sara <sarakirving@gmail.com>
2020-10-30 14:07:53 -0700

    removed the unused handleClick function and remove reference to it from the Box components that display the Corgi or Unicorn images

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,46 +1,8 @@
 import React, {useEffect} from 'react';
 import Corgi from './Corgi';
 import Unicorn from './Unicorn';
-import useGameState from '../hooks/useGameState';
 
 export default function Box (props) {
-
-  //const {gameState, setGameState} = useGameState();
-
-  function handleBoxClick () {
-    // if (props.image === "U") {
-    //   return <Unicorn/>
-    // } else if (props.image === "N") {
-    //   return null;
-    // }
-
-    console.log("YOU clicked a BOX!")
-    //console.log("unicornTurn before setState is = ", gameState.unicornTurn)
-
-    // useEffect(() => {
-    //   setGameState(prev => {return {...prev, unicornTurn: false, score: {unicorn: 7, corgi:7}}})
-
-    // })
-    // if (gameState.unicornTurn === true) {
-    //   setGameState(prev => {return {...prev, unicornTurn: false, score: {unicorn: 7, corgi:7}}})
-    // } else if (gameState.unicornTurn === false) {
-      
-    //     setGameState(prev => {return {...prev, unicornTurn: true, score: {unicorn: 6, corgi:6}}})
-      
-    // }
-
-    
-  
-    //console.log("unicornTurn after setState is = ", gameState.unicornTurn)
-
-    // if (gameState.unicornTurn) {
-    //   return <Unicorn/>
-    // }
-   
-    
-
-
-  }
 
   function determineTurn () {
     if (props.gameState.unicornTurn === true) {
@@ -61,10 +23,8 @@ export default function Box (props) {
   const boxImage = determineBoxImage();
 
   function updateGameBoard() {
-    //get the current gameBoar
     let updatedGameBoard = props.gameState.gameBoard.slice();
     let currentPlayer = props.gameState.unicornTurn ? "U" : "C";
-    console.log('currnetPlayer = ', currentPlayer)
     updatedGameBoard[props.id] = {...updatedGameBoard[props.id], display: currentPlayer}
     return updatedGameBoard;
   }
@@ -72,8 +32,8 @@ export default function Box (props) {
   
   return (
     <div>
-      {props.image === "U" && <button className="boxButton" onClick={event =>handleBoxClick()}><Unicorn/></button>}
-      {props.image === "C" && <button className="boxButton" onClick={event =>handleBoxClick()}><Corgi/></button>}
+      {props.image === "U" && <button className="boxButton" ><Unicorn/></button>}
+      {props.image === "C" && <button className="boxButton" ><Corgi/></button>}
       {props.image === "N" && <button className="boxButton" onClick={event => props.setGameState(prev => {return {...prev, unicornTurn: turn, gameBoard: updateGameBoard()}})}>{boxImage}</button>}
     </div>
   
